534efe8f2ecdbf33e1ce30102c57b26acec4512a
Vijay-Dahiya <vijaydahiyano.1@gmail.com>
2021-07-23 18:06:17 +0530

    Vijay

---
+++ Paytm Mall/app/src/main/java/com/example/paytmmall/ProductViewer_Activity.java
@@ -0,0 +1,14 @@
+package com.example.paytmmall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductViewer_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_viewer);
+    }
+}
---
+++ Paytm Mall/app/src/main/java/com/example/paytmmall/View_Device.java
@@ -0,0 +1,14 @@
+package com.example.paytmmall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Device extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_device);
+    }
+}

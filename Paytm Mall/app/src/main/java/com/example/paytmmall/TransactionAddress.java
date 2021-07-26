20bbc69599f8e03c3a808d47206f9428a0a91711
dell <abhimanyumishra130@gmail.com>
2021-07-26 16:20:26 +0530

    abhimanyu

--- Paytm Mall/app/src/main/java/com/example/paytmmall/TransactionAddress.java
+++ Paytm Mall/app/src/main/java/com/example/paytmmall/TransactionAddress.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
@@ -13,11 +14,13 @@ public class TransactionAddress extends AppCompatActivity {
 
     private Button goToPay;
     private ImageView ivBack;
+    private EditText ivName,ivNumber,ivPin,ivHomeNo,ivLocality;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_address);
+        initView();
 
         goToPay = findViewById(R.id.goToPay);
         ivBack = findViewById(R.id.ivBackArrow);
@@ -25,8 +28,10 @@ public class TransactionAddress extends AppCompatActivity {
         goToPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TransactionAddress.this, ProceedToPayPage.class);
-                startActivity(intent);
+                if(isName() && isNumber() && isPinCode() && isHomeNo() && isLocality()) {
+                    Intent intent = new Intent(TransactionAddress.this, ProceedToPayPage.class);
+                    startActivity(intent);
+                }
             }
         });
         ivBack.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +42,61 @@ public class TransactionAddress extends AppCompatActivity {
             }
         });
 
+    }
+
+    private boolean isLocality() {
+
+        if(ivLocality.getText().toString().length()!=0){
+            return true;
+        }else{
+            ivLocality.setError("This section can not be empty");
+        }
+        return false;
+    }
+
+    private boolean isHomeNo() {
+        if(ivHomeNo.getText().toString().length()!=0){
+            return true;
+        }else{
+            ivHomeNo.setError("This section can not be empty");
+        }
+        return false;
+    }
+
+    private boolean isPinCode() {
+        if(ivPin.getText().toString().length()==6){
+            return true;
+        }else{
+            ivPin.setError("Enter valid pin code");
+        }
+        return false;
+
+    }
+
+    private boolean isNumber() {
+        if(ivNumber.getText().toString().length()==10){
+            return true;
+        }else{
+            ivNumber.setError("Enter valid number");
+        }
+        return false;
+    }
+
+    private boolean isName() {
+        if(ivName.getText().toString().length()!=0){
+            return true;
+        }else{
+            ivName.setError("Enter your name");
+        }
+        return false;
+    }
+
+    private void initView() {
+        ivName=findViewById(R.id.IvName);
+        ivNumber=findViewById(R.id.IvNumber);
+        ivPin=findViewById(R.id.IvPincode);
+        ivHomeNo=findViewById(R.id.IvHomeNo);
+        ivLocality=findViewById(R.id.IvLocality);
     }
 
 
